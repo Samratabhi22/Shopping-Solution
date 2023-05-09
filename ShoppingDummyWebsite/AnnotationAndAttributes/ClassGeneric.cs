5790523871556e498a05c2fc05c58c258c12ecf4
Samratabhi <mech.abhi22@gmail.com>
2023-05-09 12:14:50 +0530

    Class without inheritance

---
+++ ShoppingDummyWebsite/AnnotationAndAttributes/ClassGeneric.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingDummyWebsite.AnnotationAndAttributes
+{
+    [TestClass]
+    public class ClassGeneric
+    {
+
+
+        [TestInitialize]
+        public void TestIni()
+        {
+            Console.WriteLine("Test Initialized");
+        }
+        [TestCleanup]
+        public void TestClean()
+        {
+            Console.WriteLine("Test Cleaned");
+        }
+
+
+    }  
+}
+
---
+++ ShoppingDummyWebsite/AnnotationAndAttributes/ClassWithoutInheritance.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingDummyWebsite.AnnotationAndAttributes
+{
+    public class ClassWithoutInheritance
+    {
+        [TestMethod]
+        public void Sample1()
+        {
+            Console.WriteLine(" Sample1 Test Method without Inheriting");
+        }
+        [TestMethod]
+        public void Sample2()
+        {
+            Console.WriteLine(" Sample2 Test Method without Inheriting");
+        }
+        [TestMethod]
+        public void Sample3()
+        {
+            Console.WriteLine(" Sample3 Test Method without Inheriting");
+        }
+    }
+}

d069537383dbb4b5ce78002be704770a6e90c9f8
Samratabhi <mech.abhi22@gmail.com>
2023-05-09 12:27:56 +0530

    ClassWithInheritance

---
+++ ShoppingDummyWebsite/AnnotationAndAttributes/ClassWithInheritance.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingDummyWebsite.AnnotationAndAttributes
+{
+    [TestClass]
+    public class ClassWithInheritance
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            Console.WriteLine(" Example1 Test Method without Inheriting");
+        }
+        [TestMethod]
+        public void Example2()
+        {
+            Console.WriteLine(" Example2 Test Method without Inheriting");
+        }
+        [TestMethod]
+        public void Example3()
+        {
+            Console.WriteLine(" Example3 Test Method without Inheriting");
+        }
+    }
+}

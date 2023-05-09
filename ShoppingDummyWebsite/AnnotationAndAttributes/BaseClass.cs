6508bae1f0ed91f1deb3fa355e273dffa9fcab73
Samratabhi <mech.abhi22@gmail.com>
2023-05-09 12:33:47 +0530

    BaseClass

---
+++ ShoppingDummyWebsite/AnnotationAndAttributes/BaseClass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingDummyWebsite.AnnotationAndAttributes
+{
+    [TestClass]
+    public class BaseClass
+    {
+        [AssemblyInitialize]
+        public static void AssemblyIni(TestContext context)
+        {
+            Console.WriteLine("Assembly iniitialized");
+        }
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            Console.WriteLine("Assembly cleanedup");
+
+        }
+    }
+}

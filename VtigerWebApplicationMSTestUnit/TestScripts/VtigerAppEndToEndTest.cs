381d913b9df053cc9a85f220ae5ab81f47a2edb7
Samratabhi <mech.abhi22@gmail.com>
2023-05-10 19:33:55 +0530

    Webdiiver

--- VtigerWebApplicationMSTestUnit/TestScripts/VtigerAppEndToEndTest.cs
+++ VtigerWebApplicationMSTestUnit/TestScripts/VtigerAppEndToEndTest.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VtigerWebApplicationMSTestUnit.POM_Pages;
+using VtigerWebApplicationMSTestUnit.Utilities;
 
 namespace VtigerWebApplicationMSTestUnit.TestScripts
 {
@@ -14,8 +15,9 @@ namespace VtigerWebApplicationMSTestUnit.TestScripts
     {
 
         public static IWebDriver driver;
+        public WebdriverUtility utility;
 
-        [ClassInitialize]
+       [ClassInitialize]
         public static  void Setup(TestContext context)
         {
             // Set up the driver before each test
@@ -25,26 +27,18 @@ namespace VtigerWebApplicationMSTestUnit.TestScripts
         public void wait()
 
         {
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(8);
+            utility = new WebdriverUtility();
+            utility.MaximizeWindow(driver);
+           
+            utility.ImplicitlyWaitingForSeconds(driver, 8);
+            
         }
         [TestMethod]
         public void Login_ValidCredentials_SuccessfulLogin()
         {
             // Navigate to the login page
             driver.Navigate().GoToUrl("http://localhost:8888/");
-
-            // Find the username and password input fields and enter valid credentials
-            //var usernameInput = driver.FindElement(By.Name("user_name"));
-            //usernameInput.SendKeys("admin");
-
-            //var passwordInput = driver.FindElement(By.Name("user_password"));
-            //passwordInput.SendKeys("admin");
-
-            // Find the login button and click it
-            //var loginButton = driver.FindElement(By.Id("submitButton"));
-            //loginButton.Click();
-
+            
             LoginPage login = new LoginPage(driver);
             login.Valid_Login("admin", "admin");
            
@@ -55,41 +49,10 @@ namespace VtigerWebApplicationMSTestUnit.TestScripts
             
         }
         [TestMethod]
+        
         public void CreateContact_ValidData_ContactCreated()
         {
-            // Login to Vtiger CRM
-            //  Login_ValidCredentials_SuccessfulLogin();
-
-
-
-            // Navigate to the login page
             
-            //driver.Navigate().GoToUrl("http://localhost:8888/");
-            // Login to Vtiger CRM
-           //  Login_ValidCredentials_SuccessfulLogin();
-
-
-            // Find the "Create" button and click it
-           
-            //IWebElement createButton = driver.FindElement(By.XPath("//a[text()='Contacts']"));
-            //createButton.Click();
-
-            // Navigate to the Contacts module
-            //IWebElement plusicon = driver.FindElement(By.XPath("//img[contains(@title,'Create Contact')]"));
-            //plusicon.Click();
-            // Fill in the contact details
-            //var firstNameInput = driver.FindElement(By.Name("firstname"));
-            //firstNameInput.SendKeys("John");
-
-            //var lastNameInput = driver.FindElement(By.Name("lastname"));
-            //lastNameInput.SendKeys("Doe");
-
-            //var emailInput = driver.FindElement(By.Name("email"));
-            //emailInput.SendKeys("john.doe@example.com");
-
-            //// Save the contact
-            //var saveButton = driver.FindElement(By.CssSelector(".crmbutton.small.save"));
-            //saveButton.Click();
 
             ContactPage contact = new ContactPage(driver);
             contact.Create_Contact("John", "Doe", "john.doe@example.com");
---
+++ VtigerWebApplicationMSTestUnit/Utilities/WebdriverUtility.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VtigerWebApplicationMSTestUnit.Utilities
+{
+    public  class WebdriverUtility
+    {
+        public void ImplicitlyWaitingForSeconds(IWebDriver driver, int seconds)
+
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(seconds);
+        }
+
+
+        public void MaximizeWindow(IWebDriver driver)
+        {
+            driver.Manage().Window.Maximize();
+        }
+    }
+}
